4866517fbba24ecc32cc8543433b727907f99b8e
Stella Zhang <stellazhang@stella-zhangs-macbook-2.local>
2019-02-07 21:09:08 -0800

    Create review schema

--- database/index.js
+++ database/index.js
@@ -8,3 +8,42 @@ db.once('open', function() {
   console.log('database connected')
 });
 
+let reviewSchema = new mongoose.Schema({
+  restaurantId: Number,
+  reviewId: Number,
+  rating: [{
+    overall: Number,
+    food: Number,
+    service: Number,
+    ambience: Number,
+    value: Number
+  }],
+  reviews: [{
+      reviewId: Number,
+      userId: Number,
+      username: String,
+      date: Date,
+      location: String,
+      reviewInfo: String,
+      rating: [{
+          overall: Number,
+          food: Number,
+          service: Number,
+          ambience: Number,
+          value: Number
+      }]
+  }]
+
+});no
+
+let Review = mongoose.model('Review', reviewSchema);
+
+Review.create({ restaurantId: 1009, reviewId: 2000 }, function (err, results) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(results)
+    }
+});
+
+module.exports Review = Review;
